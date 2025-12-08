d5bd77d5c0870d563699187d57422ca4f0c6be8e
Ash Carter <acarter010204@gmail.com>
2025-12-08 14:25:48 +1100

    leson 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    println!("Hello, world!");
+    // Config
+
+    let image_width = 256;
+    let image_height = 256;
+
+    // Render
+
+    print!("P3\n {image_width} {image_height} \n255\n");
+
+    for j in 0..image_height  {
+        for i in 0..image_width {
+            let r = i as f64 / (image_width-1) as f64;
+            let g = j as f64 / (image_height-1) as f64;
+            let b = 0.0f64;
+
+            let ir = (255.999 * r) as i64;
+            let ig = (255.999 * g) as i64;
+            let ib = (255.999 * b) as i64;
+
+            print!("{ir} {ig} {ib}");
+        }
+    }
+
 }
