e3783b04f32ee0d675664b55d9f00317dc443d30
Ash Carter <acarter010204@gmail.com>
2025-12-12 21:34:18 +1100

    added simple time profiling and removed time variable random code

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,14 @@ mod sphere;
 mod utils;
 mod vec3;
 
-use std::{f64::consts::PI, rc::Rc};
+use std::rc::Rc;
+use std::time::Instant;
 
 use camera::Camera;
 use hittable::HittableList;
 use material::{Dielectric, Lambertian, Material, Metal};
 use sphere::Sphere;
-use vec3::{Color, Vec3};
-
-use crate::{
-    utils::{random_double, random_double_range},
-    vec3::Point3,
-};
+use vec3::{Color, Point3, Vec3};
 
 fn main() {
     let mut world = HittableList::new();
@@ -35,12 +31,8 @@ fn main() {
 
     for a in -11..11 {
         for b in -11..11 {
-            let choose_mat = random_double();
-            let center = Point3::new(
-                a as f64 + 0.9 * random_double(),
-                0.2,
-                b as f64 + 0.9 * random_double(),
-            );
+            let choose_mat = (a + b + 22) as f64 / 44.0;
+            let center = Point3::new(a as f64 + 0.9 * 0.5, 0.2, b as f64 + 0.9 * 0.5);
 
             if (&center - &Point3::new(4.0, 0.2, 0.0)).len() > 0.9 {
                 let sphere_material: Option<Rc<dyn Material>> = if choose_mat < 0.8 {
@@ -50,7 +42,7 @@ fn main() {
                 } else if choose_mat < 0.95 {
                     // Metal
                     let albedo = Color::random_from_range(0.5, 1.0);
-                    let fuzz = random_double_range(0.0, 0.5);
+                    let fuzz = 0.5;
                     Some(Rc::new(Metal::new(albedo, fuzz)))
                 } else {
                     // Glass
@@ -87,9 +79,9 @@ fn main() {
     // Camera setup
     let mut camera = Camera::default();
     camera.aspect_ratio = 16.0 / 9.0;
-    camera.image_width = 1200;
-    camera.samples_per_pixel = 500;
-    camera.max_depth = 50;
+    camera.image_width = 600;
+    camera.samples_per_pixel = 10;
+    camera.max_depth = 10;
 
     camera.vfov = 20.0;
     camera.lookfrom = Point3::new(13.0, 2.0, 3.0);
@@ -100,5 +92,8 @@ fn main() {
     camera.focus_dist = 10.0;
 
     // Render
+    let start = Instant::now();
     camera.render(&world);
+    let duration = start.elapsed();
+    eprintln!("render time: {:?}", duration);
 }
