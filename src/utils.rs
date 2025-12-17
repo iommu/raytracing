009a77817a5ea71fb0fc3fe8ec378c745cef85bd
Ash Carter <acarter010204@gmail.com>
2025-12-18 00:37:58 +1100

    added static small RNG for much faster runtime

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,15 @@
-use rand::Rng;
+use lazy_static::lazy_static;
+use rand::rngs::SmallRng;
+use rand::{Rng, SeedableRng};
 use std::f64::consts::PI;
+use std::sync::Mutex;
+
+lazy_static! {
+    static ref RNG: Mutex<SmallRng> = {
+        // Initialize StdRng using a system-provided seed (cryptographically secure)
+        Mutex::new(SmallRng::from_rng(&mut rand::rng()))
+    };
+}
 
 #[allow(dead_code)]
 #[inline(always)]
@@ -11,16 +21,18 @@ pub fn degrees_to_radians(degrees: f64) -> f64 {
 #[inline(always)]
 pub fn random_double() -> f64 {
     // Returns a random real in [0,1).
-    let mut rng = rand::rng();
-    rng.random_range(0.0..1.0)
+    RNG.lock()
+        .expect("Failed to lock the global RNG Mutex")
+        .random_range(0.0..1.0)
 }
 
 #[allow(dead_code)]
 #[inline(always)]
 pub fn random_double_range(min: f64, max: f64) -> f64 {
     // Returns a random real in [min,max).
-    let mut rng = rand::rng();
-    rng.random_range(min..max)
+    RNG.lock()
+        .expect("Failed to lock the global RNG Mutex")
+        .random_range(min..max)
 }
 
 #[allow(dead_code)]
@@ -28,8 +40,9 @@ pub fn random_double_range(min: f64, max: f64) -> f64 {
 pub fn random_int_range(min: i32, max: i32) -> i32 {
     let max = max + 1;
     // Returns a random real in [min,max+1).
-    let mut rng = rand::rng();
-    rng.random_range(min..max)
+    RNG.lock()
+        .expect("Failed to lock the global RNG Mutex")
+        .random_range(min..max)
 }
 
 #[allow(dead_code)]
