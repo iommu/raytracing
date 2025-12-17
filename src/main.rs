0b6e3d3bb0a470964e95f3dba8346ee75c8d040a
Ash Carter <acarter010204@gmail.com>
2025-12-17 23:16:24 +1100

    final scene

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 mod aabb;
 mod bvh;
 mod camera;
+mod constant_medium;
 mod exporter;
 mod hittable;
 mod interval;
@@ -15,7 +16,6 @@ mod texture;
 mod tri;
 mod utils;
 mod vec3;
-mod constant_medium;
 
 use std::time::Instant;
 use std::{io, rc::Rc};
@@ -26,6 +26,7 @@ use exporter::{BMPExporter, Exporter};
 use hittable::HittableList;
 use material::{Dielectric, Lambertian, Material, Metal};
 use sphere::Sphere;
+use stb_image::image;
 use utils::{random_double, random_double_range};
 use vec3::{Color, Point3, Vec3};
 
@@ -35,6 +36,134 @@ use crate::material::DiffuseLight;
 use crate::quad::{Quad, box_new};
 use crate::texture::{CheckerTexture, ImageTexture, NoiseTexture, Texture};
 use crate::tri::Tri;
+
+fn final_scene(image_width: i32, samples_per_pixel: i32, max_depth: i32) -> io::Result<()> {
+    let exporter: Box<dyn Exporter> = Box::new(BMPExporter::new("render.bmp")?);
+
+    let mut boxes_1 = HittableList::default();
+    let ground = Rc::new(Lambertian::from_color(Color::new(0.48, 0.83, 0.53)));
+
+    let boxes_per_side = 20;
+    for i in 0..boxes_per_side {
+        for j in 0..boxes_per_side {
+            let w = 100.0;
+            let x0 = -1000.0 + i as f64 * w;
+            let z0 = -1000.0 + j as f64 * w;
+            let y0 = 0.0;
+            let x1 = x0 + w;
+            let y1 = random_double_range(1.0, 101.0);
+            let z1 = z0 + w;
+
+            boxes_1.add(box_new(
+                Point3::new(x0, y0, z0),
+                Point3::new(x1, y1, z1),
+                ground.clone(),
+            ));
+        }
+    }
+
+    let mut world = HittableList::default();
+
+    world.add(Rc::new(BVHNode::from_list(boxes_1)));
+
+    let light = Rc::new(DiffuseLight::from_color(Color::new(7.0, 7.0, 7.0))) as Rc<dyn Material>;
+    world.add(Rc::new(Quad::new(
+        Vec3::new(123.0, 554.0, 147.0),
+        Vec3::new(300.0, 0.0, 0.0),
+        Vec3::new(0.0, 0.0, 265.0),
+        light.clone(),
+    )));
+
+    let center_1 = Point3::new(400.0, 400.0, 200.0);
+    let center_2 = center_1 + Vec3::new(30.0, 0.0, 0.0);
+    let sphere_material = Rc::new(Lambertian::from_color(Color::new(0.7, 0.3, 0.1)));
+    world.add(Rc::new(Sphere::new_moving(
+        center_1,
+        center_2,
+        50.0,
+        sphere_material,
+    )));
+
+    world.add(Rc::new(Sphere::new_stationary(
+        Vec3::new(260.0, 150.0, 45.0),
+        50.0,
+        Rc::new(Dielectric::new(1.5)),
+    )));
+    world.add(Rc::new(Sphere::new_stationary(
+        Vec3::new(0.0, 150.0, 145.0),
+        50.0,
+        Rc::new(Metal::new(Color::new(0.8, 0.8, 0.8), 1.0)),
+    )));
+
+    let boundary = Rc::new(Sphere::new_stationary(
+        Point3::new(360.0, 150.0, 145.0),
+        70.0,
+        Rc::new(Dielectric::new(1.5)),
+    ));
+    world.add(boundary.clone());
+    world.add(Rc::new(ConstantMedium::from_color(
+        boundary.clone(),
+        0.2,
+        Color::new(0.2, 0.4, 0.9),
+    )));
+    let boundary = Rc::new(Sphere::new_stationary(
+        Point3::new(0.0, 0.0, 0.0),
+        5000.0,
+        Rc::new(Dielectric::new(1.5)),
+    ));
+    world.add(Rc::new(ConstantMedium::from_color(
+        boundary.clone(),
+        0.0001,
+        Color::new(1.0, 1.0, 1.0),
+    )));
+
+    let emat = Rc::new(Lambertian::new(Rc::new(ImageTexture::new("earthmap.jpg"))));
+    world.add(Rc::new(Sphere::new_stationary(
+        Point3::new(400.0, 200.0, 400.0),
+        100.0,
+        emat,
+    )));
+    let pertext = Rc::new(NoiseTexture::new(0.2));
+    world.add(Rc::new(Sphere::new_stationary(
+        Point3::new(220.0, 280.0, 300.0),
+        80.0,
+        Rc::new(Lambertian::new(pertext)),
+    )));
+
+    let mut boxes_2 = HittableList::default();
+    let white = Rc::new(Lambertian::from_color(Color::new(0.73, 0.73, 0.73)));
+    let ns = 1000;
+    for j in 0..ns {
+        boxes_2.add(Rc::new(Sphere::new_stationary(
+            Point3::random_from_range(0.0, 165.0),
+            10.0,
+            white.clone(),
+        )));
+    }
+    world.add(Rc::new(Translate::new(
+        Rc::new(RotateY::new(Rc::new(BVHNode::from_list(boxes_2)), 15.0)),
+        Vec3::new(-100.0, 270.0, 395.0),
+    )));
+
+    let mut camera = Camera::from_exporter(exporter);
+    camera.aspect_ratio = 1.0;
+    camera.image_width = image_width;
+    camera.samples_per_pixel = samples_per_pixel;
+    camera.max_depth = max_depth;
+    camera.background = Color::new(0.0, 0.0, 0.0);
+
+    camera.vfov = 40.0;
+    camera.lookfrom = Point3::new(478.0, 278.0, -600.0);
+    camera.lookat = Point3::new(278.0, 278.0, 0.0);
+    camera.vup = Vec3::new(0.0, 1.0, 0.0);
+
+    camera.defocus_angle = 0.0;
+
+    // Render
+    camera.render(&world);
+
+    Ok(())
+}
 
 fn cornell_smoke() -> io::Result<()> {
     let exporter: Box<dyn Exporter> = Box::new(BMPExporter::new("render.bmp")?);
@@ -96,7 +225,11 @@ fn cornell_smoke() -> io::Result<()> {
     );
     let box_1 = Rc::new(RotateY::new(box_1, 15.0));
     let box_1 = Rc::new(Translate::new(box_1, Vec3::new(265.0, 0.0, 295.0)));
-    world.add(Rc::new(ConstantMedium::from_color(box_1, 0.01, Color::new(0.0, 0.0, 0.0))));
+    world.add(Rc::new(ConstantMedium::from_color(
+        box_1,
+        0.01,
+        Color::new(0.0, 0.0, 0.0),
+    )));
 
     let box_2 = box_new(
         Point3::new(0.0, 0.0, 0.0),
@@ -105,7 +238,11 @@ fn cornell_smoke() -> io::Result<()> {
     );
     let box_2 = Rc::new(RotateY::new(box_2, -18.0));
     let box_2 = Rc::new(Translate::new(box_2, Vec3::new(130.0, 0.0, 65.0)));
-    world.add(Rc::new(ConstantMedium::from_color(box_2, 0.01, Color::new(1.0, 1.0, 1.0))));
+    world.add(Rc::new(ConstantMedium::from_color(
+        box_2,
+        0.01,
+        Color::new(1.0, 1.0, 1.0),
+    )));
 
     let mut camera = Camera::from_exporter(exporter);
     camera.aspect_ratio = 1.0;
@@ -501,7 +638,10 @@ fn bouncing() -> io::Result<()> {
 }
 
 fn main() -> io::Result<()> {
-    cornell_smoke()
+    final_scene(800, 10000, 40)
+
+    // final_scene(400, 250, 4)
+    // cornell_smoke()
     // cornell_box()
     // simple_lights()
     // quads()
